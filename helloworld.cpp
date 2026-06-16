7d6dc85f803323183c57f4876653154373917ebb
Ksenia Kulish <m1ss.darling@yandex.ru>
2026-06-16 18:01:32 +0300

    updated

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  // подключение библиотек 
+
+using namespace std;  // стандартное пространство имен
+
+int main() {
+    // cout - поток вывода, << - запись в поток
+    float num;
+    num = 456.456;
+    cout << "Hello, world!\n";
+    cout << num << '\n';
+}
+
